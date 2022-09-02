7896677052d8e3f21b8072defd37e6df1be4b82e
AntonKravkenko <antonKravkenko@gmail.com>
2022-09-02 21:00:59 +0200

    add delete button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useEffect } from 'react';
 import './App.css';
+import * as Scroll from 'react-scroll';
 import React from 'react';
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 function App() {
+  const axios = require('axios');
+
   const [todos, setTodos] = React.useState([])
-  const [titles, settitle] = React.useState("");
-  const [bodys, setbodys] = React.useState("");
+  const [title, settitle] = React.useState("");
+  const [body, setbodys] = React.useState("");
   const [Isloading, setLoading] = React.useState(false);
   
   useEffect(() => {
@@ -15,21 +18,34 @@ function App() {
     }, 2000);
   }, []);
             
-    const deleted = id => setTodos([...todos].filter(t => t.id !== id));
+    const deleted = index => {
+      const copy = [...todos]
+     copy.splice(index, 1);
+     console.log(copy)
+     setTodos(copy)
+   
+      
+    }
   const addTodo = () => {
 		setTodos(todos => [
 			{
-				titles,
-				bodys,
+				title,
+				body,
 			},
 			...todos,   
 		])
    settitle("")
    setbodys("")
+
  
 	}
 
+async function FetchPost() {
+  const {data} = await axios.get("https://jsonplaceholder.typicode.com/posts")
+  setTodos(data)
+ 
 
+ }
   return (
     <div className="App">
    { Isloading ? (
@@ -47,21 +63,24 @@ function App() {
 
      ) : (
       <body>
-      <h1>First Project - TodoApp</h1>
-       {  todos.map(todo1 => 
+      <h1 onClick={FetchPost}>First Project - TodoApp</h1>
+       {  todos.map((todo1, index) => ( 
 <div className='mapingdivs'>
 <div className='widthBLOCK'>
-<p> {todo1.titles}</p>
- <p> {todo1.bodys}</p>
+
+ <p className='m5'> {todo1.title}</p>
+ <p className='bodys'> {todo1.body}</p>
+ 
+
 </div>
 
- <button className='delete' onClick={deleted}>Удалить</button>
+ <button className='delete' onClick={() => deleted(index)}>Удалить</button>
  
-  </div>)}
+  </div>))}
 
 <div className='addTodoFild'>
-<input placeholder='titles' value={titles} onChange={e => settitle(e.target.value)} ></input>
-<input placeholder='body' value={bodys} onChange={e => setbodys(e.target.value)} ></input>
+<input placeholder='titles' value={title} onChange={e => settitle(e.target.value)} ></input>
+<input placeholder='body' value={body} onChange={e => setbodys(e.target.value)} ></input>
 <button onClick={addTodo}>добавить</button>
 </div>
  </body> )}
