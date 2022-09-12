82db68cc801a1439dd8040cf034a28427936c65c
AntonKravkenko <antonKravkenko@gmail.com>
2022-09-12 08:20:29 +0200

    delete preloader and add localstorage

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import { useEffect } from 'react';
 import './App.css';
+import { initializeApp } from "firebase/app";
 import { FaTrashAlt } from 'react-icons/fa';
+import { MdDeleteSweep } from 'react-icons/md';
 import React from 'react';
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 function App() {
+ 
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAk1rZbPAifRK-etQdKossW8l_aljJp3Zo",
+    authDomain: "todo-app-c51aa.firebaseapp.com",
+    projectId: "todo-app-c51aa",
+    storageBucket: "todo-app-c51aa.appspot.com",
+    messagingSenderId: "868228809297",
+    appId: "1:868228809297:web:b2a242c227a03bf2b12e70",
+    measurementId: "G-6R8STXHX8D"
+  };
+  const app = initializeApp(firebaseConfig);
   const axios = require('axios');
-  const [todos, setTodos] = React.useState([])
+  const [todos, setTodos] = React.useState(JSON.parse(localStorage.getItem('todos')) || [])
   const [title, settitle] = React.useState("");
+  const [Search, setSearch] = React.useState("");
   const [Isloading, setLoading] = React.useState(false);
   const [HeaderText__value, setHeaderText] = React.useState("TodoApp in react")
-
   async function FetchPost() {
     setHeaderText("You load todos from jsonplaceholder");
-    const {data} = await axios.get("https://jsonplaceholder.typicode.com/posts")
+    const {data} = await axios.get("https://jsonplaceholder.typicode.com/todos")
   setTodos(data)
-  
   }
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() =>   {
-      setLoading(false)
-    }, 2000);
-  }, []);
+  
             
     const deleted = index => {
       const copy = [...todos]
      copy.splice(index, 1);
- 
-     setTodos(copy)
+     setTodos(copy);
    
-      
     }
-
+    localStorage.setItem('todos', JSON.stringify(todos));
   const addTodo = () => {
     if(title === '') {
      console.log("write title n field")
     } else {
+      
       setTodos(todos => [
         {
           title,
@@ -44,32 +49,29 @@ function App() {
         ...todos,   
       ])
      settitle("")
+   
     }
-	
-
- 
 	}
-
+  localStorage.setItem('todos', JSON.stringify(todos));
+  const DeleteAllTodos = () => {
+    localStorage.removeItem('todos', JSON.stringify(todos));
+    setTodos([])}
 
   return (
     <body className="App">
-   { Isloading ? (
+  
 
-    <ClimbingBoxLoader
-  height="300"
-  className='loader'
-  width="300"
-  radius="100"
-  color="white"
-
-/>
+  
    
-
-     ) : (
       <div>
-      <h1 onClick={FetchPost}>{HeaderText__value}</h1>
+     <div className='first__Block__Wrapeer'>
+     <h1 onClick={FetchPost}>{HeaderText__value}</h1>
+      <MdDeleteSweep className='trash__Delete__All_Todos' onClick={DeleteAllTodos}></MdDeleteSweep>
+     </div>
+      <input className="Search__input" placeholder='Search' value={Search} onChange={e => setSearch(e.target.value.toLowerCase())}></input>
     <div className="mapping__elements__container">
-    {  todos.map((todo1, index) => ( 
+  
+    {  todos.filter(todos => todos.title.toLowerCase().includes(Search)).map((todo1, index) => ( 
 <div className='mapingdivs' key={index}>
 <div className='widthBLOCK'>
  <p className='m5' > {todo1.title}</p>
@@ -80,9 +82,10 @@ function App() {
 
 <div className='input__Wrapper'>
 <input placeholder='Write you todo here' onKeyPress={(e) => e.key === 'Enter' && addTodo()} value={title} onChange={e => settitle(e.target.value)} ></input>
+
 </div>
  </div> 
- )}
+
  </body> ); }
 
 export default App;
